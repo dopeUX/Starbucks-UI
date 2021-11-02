c41122b8bd244b9a3d70e4ad98bc1af9720885dc
UXNation <93445494+UXNation@users.noreply.github.com>
2021-11-02 13:47:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
-
 import Dashboard from './components/Dashboard';
-import CoffeeDetails from './components/CoffeeDetails';
-import reactNotificationsComponent from 'react-notifications-component';
-
-
 import ReactNotification from 'react-notifications-component';
 import 'react-notifications-component/dist/theme.css';
-import { store } from 'react-notifications-component';
+
 
 function App(){
     return (
