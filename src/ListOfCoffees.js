bece2cfae8cc8391e1428e2944371b0493448c9d
UXNation <93445494+UXNation@users.noreply.github.com>
2021-11-02 13:50:47 +0530

    Update ListOfCoffees.js

--- src/ListOfCoffees.js
+++ src/ListOfCoffees.js
@@ -2,21 +2,21 @@ const ListOfCoffee = [
     {
       id: 1,
       name: "Strawberry Funnel",
-      image : "./images/coffee1.svg",
+      image : "./images/coffee1.png",
       rating: 4.2,
       price:"$ 39.00"
     },
     {
         id: 2,
         name: "Caramel Ribbon",
-        image : "./images/coffee2.svg",
+        image : "./images/coffee2.png",
         rating: 3.5,
         price:"$ 45.00"
       },
       {
         id: 3,
         name: "Pumpkin Spice",
-        image : "./images/coffee3.svg",
+        image : "./images/coffee3.png",
         rating: 4.5,
         price:"$ 35.00"
       },
