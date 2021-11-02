1991d9fcf0377de281c061bd1675869b6f919ab9
UXNation <93445494+UXNation@users.noreply.github.com>
2021-11-02 13:38:32 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,18 +2,14 @@ import React from 'react';
 import AllCoffees from './CreateCoffee';
 import CoffeeDetails from './CoffeeDetails';
 import ListOfCoffee from '../ListOfCoffees';
-import { useState, useEffect}  from 'react';
+import { useState }  from 'react';
 
 let currentIndex = 1;
 
 
 function Dashboard(){
   const [index, setIndex] = useState(0);
-  
-  function changeIndex(i){
-     setIndex(i);
-     //console.log('change index');
-  }
+ 
 
   return <div className="dashboard-page">
    <img className="logo-big" src="/images/logobig.png"></img>
